c19841234d9c6700bbfe9d863ef773a0f8e6d14f
LimByeonghyeon <73628822+LimByeonghyeon@users.noreply.github.com>
2023-02-23 10:57:01 +0900

    Add files via upload

---
+++ java_study/src/java_study/A.java
@@ -0,0 +1,14 @@
+package java_study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class A {
+	public static void main(String[] args) {
+		int age = 33;
+		while(age==33) {
+			System.out.println("ddd");
+		}
+	}
+}
